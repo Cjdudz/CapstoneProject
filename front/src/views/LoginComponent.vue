10e82ffd2eebe1af716d457ee68a14024e15ff33
Cjdudz <cjdudas17@gmail.com>
2023-11-16 14:16:29 +0800

    Frontend login and logout

---
+++ front/src/views/LoginComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+  
+  
+      <!-- Include the LoginView component -->
+      <Login />
+  
+      <!-- Other content of the HomeView component -->
+      <HelloWorld />
+    </div>
+  </template>
+  
+  <script>
+  import { defineComponent } from 'vue';
+  
+  // Components
+  import HelloWorld from '../components/HelloWorld.vue';
+  import Login from '../components/Login.vue'; // Adjust the path as needed
+  
+  export default defineComponent({
+    name: 'HomeView',
+  
+    components: {
+      HelloWorld,
+      Login,
+    },
+  });
+  </script>
